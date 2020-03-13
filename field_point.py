12c3d1de94589fcb50edad7610c864603fcfe28c
Ilya Nazarenko <ilya.nazarenko@gorparkovki.ru>
2020-03-13 18:10:03 +0300

    Use i,j coord notation instead x,y on Points class.

--- field_point.py
+++ field_point.py
@@ -1,27 +1,32 @@
-TILE_SHIP_DAMAGED = '#'
-TILE_SHIP_DESTROYED = '*'
+TILE_SHIP_DAMAGED = '*'
+TILE_SHIP_DESTROYED = '#'
 TILE_SEA = '~'
 TILE_FOG_OF_WAR = '▢'
 class Point:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = x
+	'''
+	Point with coordinates in {i,j} notation
+	'''
+	def __init__(self, i, j):
+		self.i = i
+		self.j = j
 	def __str__(self):
-	 return '({0};{1})'.format(self.x,self.y)
+	 return '({0};{1})'.format(self.i,chr(ord('А') + self.j))
 class Ship:
-	def __init__(self, size, points):
-		self.size = x
+	def __init__(self, size):
+		self.size = size
 		self.damage = 0
 		# self.points = points		
+	@property
 	def isDestroyd(self):
-		if (self.damaged == self.size):
-			return True
-		else:
-			return False
+		return self.damage == self.size
+	def demage(self):
+		if (self.damage < self.size):
+			self.damage+=1
+		
 
 class FieldPoint(Point):
-	def __init__(self, x, y, ship):
-		super(FieldPoint, self).__init__(x,y)
+	def __init__(self, i, j, ship):
+		super(FieldPoint, self).__init__(i,j)
 		self.touched = False
 		self.ship = ship
 	def getSymbol(self):
@@ -29,10 +34,17 @@ class FieldPoint(Point):
 			return TILE_FOG_OF_WAR
 		elif (self.ship == None):
 			return TILE_SEA
+		elif (self.ship.isDestroyd):
+			return TILE_SHIP_DESTROYED
+		else:
+			return TILE_SHIP_DAMAGED
+	
+
 	def touch(self):
-		self.touched = True
-		if (self.ship != None):
-			self.ship.damage += 1
+		if (not self.touched):
+			self.touched = True
+			if (self.ship != None):
+				self.ship.damage += 1
 point = FieldPoint(0,0, 'ship')
-print(point.x)
-print(point.y)
+print(point.i)
+print(point.j)
